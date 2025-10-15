1fb83edf9b0a53619e8cec2074a1e014c63c121d
Christian Rafael <christianparadix@gmail.com>
2025-10-15 00:50:51 -0300

    Compare analysis

--- tf_idf.py
+++ tf_idf.py
@@ -13,15 +13,33 @@
 #   some might be more important
 # Solution: Assign more weight to rare words than to common words
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
 text_clean = pd.read_pickle('Data/text_clean.pkl')
 
-tv = TfidfVectorizer(stop_words='english', ngram_range=(1,2), min_df=2)
+tv = TfidfVectorizer(stop_words='english', min_df=0.1, max_df=.5)
 tfidf = tv.fit_transform(text_clean)
 tfidf_df = pd.DataFrame(tfidf.toarray(), columns=tv.get_feature_names_out())
-print(tfidf_df)
 
+top_weights = tfidf_df.sum().sort_values().tail(10)
+top_weights.plot(kind='barh')
+plt.savefig('top_weights.png')
+
+cv = CountVectorizer(stop_words='english', ngram_range=(1,2), min_df=.2, max_df=.8)
+dtm = cv.fit_transform(text_clean)
+dtm_df = pd.DataFrame(dtm.toarray(), columns=cv.get_feature_names_out())
+
+term_freq = dtm_df.sum().sort_values().tail(10)
+
+fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10, 8))
+term_freq.plot(kind='barh', ax=axes[0], color='skyblue')
+axes[0].set_title('Count Vectorizer')
+
+top_weights.plot(kind='barh', ax=axes[1], color='salmon')
+axes[1].set_title('TF-IDF Vectorizer')
+
+plt.tight_layout()
+plt.savefig('analysis.png')
